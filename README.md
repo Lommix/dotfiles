d37601da06aad93170d8c378f2bcd4cbe12b79ab
Lorenz Mielke <info@lommix.de>
2022-08-10 13:17:47 +0200

    as symlink

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-My linux configs
-
-chmod +x install.sh
-./install.sh
-
-(overwrites existing configs)
-
+My dotfiles
--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 echo "overwriting dotfiles"
 
 #tmux
-cp .tmux.conf ~/.tmux.conf
+rm -f "${HOME}"/.tmux.conf
+ln -sr .tmux.conf "${HOME}"/.tmux.conf
+
 
 #nvim
-cp -rf nvim ~/.config/nvim
+
+rm -f "${HOME}"/.config/nvim/init.vim
+rm -f "${HOME}"/.config/nvim/plugin/coc.vim
+
+ln -sr nvim/init.vim "${HOME}"/.config/nvim/init.vim
+ln -sr nvim/plugin/coc.vim "${HOME}"/.config/nvim/plugin/coc.vim
