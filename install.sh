3e527c5c46cfcca68b82ae31e4b521a46a0aeb55
Lorenz Mielke <info@lommix.de>
2022-09-09 01:06:14 +0200

    updated install

--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
 
-
+#tmux
 rm -f "${HOME}"/.tmux.conf
 ln -s "$BASEDIR"/.tmux.conf "${HOME}"/.tmux.conf
 
 #nvim
-rm -f "${HOME}"/.config/nvim/init.vim
-rm -f "${HOME}"/.config/nvim/plugin/coc.vim
-rm -f "${HOME}"/.config/nvim/plugin/telescope.vim
-
-ln -s "$BASEDIR"/nvim/init.vim "${HOME}"/.config/nvim/init.vim
-ln -s "$BASEDIR"/nvim/plugin/coc.vim "${HOME}"/.config/nvim/plugin/coc.vim
-ln -s "$BASEDIR"/nvim/plugin/telescope.vim "${HOME}"/.config/nvim/plugin/telescope.vim
+rm -rf "{$HOME}"/.config/nvim
+ln -s "$BASEDIR"/nvim "{HOME}"/.config/nvim
 
 echo "cloning tmux plugin manager"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
