5ab450aa42a41febf6bf1a57a3d6c0f7e1dca8a2
Lorenz Mielke <info@lommix.de>
2022-10-24 16:46:33 +0200

    wip

---
+++ scripts/monitor_setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --mode 2560x1440 --pos 1920x0 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 7040x0 --rotate normal --output DisplayPort-2 --primary --mode 2560x1440 --pos 4480x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal
