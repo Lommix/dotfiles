ce3a8c7cd8011523f8260f15b89492b9293d0ef0
Lorenz Mielke <info@lommix.de>
2023-01-24 19:03:00 +0100

    wip

--- nvim/lua/lommix/plugins.lua
+++ nvim/lua/lommix/plugins.lua
@@ -20,9 +20,20 @@ return require("packer").startup(function(use)
 	use({
 		"Exafunction/codeium.vim",
 		config = function()
+			vim.g.codeium_disable_bindings = true
 			vim.keymap.set("i", "<C-a>", function()
 				return vim.fn["codeium#Accept"]()
 			end, { expr = true })
+			vim.keymap.set("i", "<C-j>", function()
+				return vim.fn["codeium#CycleCompletions"](1)
+			end, { expr = true })
+			vim.keymap.set("i", "<C-k>", function()
+				return vim.fn["codeium#CycleCompletions"](-1)
+			end, { expr = true })
+			vim.g.codeium_filetypes = {
+				gdscript = false,
+				sh = false,
+			}
 		end,
 	})
 	-- ui
