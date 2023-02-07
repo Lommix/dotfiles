7bccc17dbc39a5853755c9ff3e2a1b3868b80249
Lorenz Mielke <info@lommix.de>
2023-02-07 14:58:58 +0100

    wip

--- nvim/lua/lommix/dap.lua
+++ nvim/lua/lommix/dap.lua
@@ -1,16 +1,26 @@
 local ok, dap = pcall(require, "dap")
 
-if not ok then
+if not ok or not ok2 then
 	return
 end
 
-local dapvirt  = require("nvim-dap-virtual-text")
+local dapvirt = require("nvim-dap-virtual-text")
 local dapui = require("dapui")
 
 dapui.setup()
 dapvirt.setup()
 
-vim.keymap.set("n", "<F3>", dapui.toggle, {})
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
+
+-- vim.keymap.set("n", "<F3>", dapui.toggle, {})
 vim.keymap.set("n", "<F4>", ":DapToggleBreakpoint<CR>", {})
 vim.keymap.set("n", "<F5>", ":DapContinue<CR>", {})
 vim.keymap.set("n", "<F6>", ":DapStepOver<CR>", {})
@@ -23,8 +33,8 @@ vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", n
 vim.fn.sign_define("DapStopped", { text = "⭐️", texthl = "", linehl = "", numhl = "" })
 
 dap.defaults.fallback.external_terminal = {
-    command = '/usr/local/bin/kitty';
-    args = {'--hold'};
+	command = "/usr/local/bin/kitty",
+	args = { "--hold" },
 }
 ------------------------------------------------------
 -- GO
@@ -33,42 +43,42 @@ require("dap-go").setup()
 ------------------------------------------------------
 -- GODOT
 dap.adapters.godot = {
-  type = "server",
-  host = '127.0.0.1',
-  port = 6006,
+	type = "server",
+	host = "127.0.0.1",
+	port = 6006,
 }
 
 dap.configurations.gdscript = {
-  {
-    type = "godot",
-    request = "launch",
-    name = "Launch scene",
-    project = "${workspaceFolder}",
-    launch_scene = true,
-  }
+	{
+		type = "godot",
+		request = "launch",
+		name = "Launch scene",
+		project = "${workspaceFolder}",
+		launch_scene = true,
+	},
 }
 ------------------------------------------------------
 -- RUST / C / C++
 dap.adapters.lldb = {
-  type = 'executable',
-  command = 'lldb-vscode-14',
-  name = 'lldb'
+	type = "executable",
+	command = "lldb-vscode-14",
+	name = "lldb",
 }
 
 dap.configurations.cpp = {
-  {
-    name = 'Launch',
-    type = 'lldb',
-    request = 'launch',
-	program = function()
-		local path = vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-		return path
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
-	runInTerminal = false,
-  },
+	{
+		name = "Launch",
+		type = "lldb",
+		request = "launch",
+		program = function()
+			local path = vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+			return path
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+		args = {},
+		runInTerminal = false,
+	},
 }
 
 dap.configurations.c = dap.configurations.cpp
@@ -77,20 +87,20 @@ dap.configurations.rust = dap.configurations.cpp
 ------------------------------------------------------
 -- C#
 dap.adapters.coreclr = {
-  type = 'executable',
-  command = 'netcoredbg',
-  args = {}
+	type = "executable",
+	command = "netcoredbg",
+	args = {},
 }
 
 dap.configurations.cs = {
-  {
-    type = "coreclr",
-    name = "launch - netcoredbg",
-    request = "launch",
-    program = function()
-        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/.godot/mono/temp/bin/Debug/', 'file')
-    end,
-  },
+	{
+		type = "coreclr",
+		name = "launch - netcoredbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/.godot/mono/temp/bin/Debug/", "file")
+		end,
+	},
 }
 
 -- TODO: finish when bored
--- nvim/lua/lommix/lspsage.lua
+++ nvim/lua/lommix/lspsage.lua
@@ -1,4 +1,47 @@
 local status, saga = pcall(require, "lspsaga")
-if (not status) then return end
+if not status then
+	return
+end
 
-saga.setup({})
+saga.setup({
+	ui = {
+		-- Currently, only the round theme exists
+		theme = "round",
+		-- This option only works in Neovim 0.9
+		title = true,
+		-- Border type can be single, double, rounded, solid, shadow.
+		border = "double",
+		winblend = 0,
+		expand = "",
+		collapse = "",
+		preview = " ",
+		code_action = "💡",
+		diagnostic = "🐞",
+		incoming = " ",
+		outgoing = " ",
+		hover = " ",
+		kind = {},
+	},
+	outline = {
+		win_position = "right",
+		win_with = "",
+		win_width = 30,
+		show_detail = true,
+		auto_preview = true,
+		auto_refresh = true,
+		auto_close = true,
+		custom_sort = nil,
+		keys = {
+			jump = "o",
+			expand_collapse = "u",
+			quit = "q",
+		},
+	},
+	lightbulb = {
+		enable = true,
+		enable_in_insert = true,
+		sign = true,
+		sign_priority = 40,
+		virtual_text = true,
+	},
+})
