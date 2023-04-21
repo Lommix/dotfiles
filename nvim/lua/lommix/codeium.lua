31a8b93d6ed8e444d48143be831b068dc8440b89
Lorenz Mielke <info@lommix.de>
2023-04-21 17:47:01 +0200

    wip

--- nvim/lua/lommix/codeium.lua
+++ nvim/lua/lommix/codeium.lua
@@ -1,8 +1,19 @@
 vim.g.codeium_disable_bindings = true
 vim.g.codeium_enabled = false
+
+
 vim.keymap.set("i", "<C-a>", function()
 	return vim.fn["codeium#Accept"]()
 end, { expr = true })
+
+
 vim.g.codeium_filetypes = {
 	sh = false,
+	bash = false,
 }
+
+-- enable by default for filetypes
+vim.api.nvim_create_autocmd({"BufEnter"}, {
+	pattern = {"*.php","*.js","*.ts","*.svelte", "*.rs"},
+	command = "CodeiumEnable",
+})
