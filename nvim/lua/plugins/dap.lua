a4a672440fe5f537bd5aee1ceca6e282ac315ad6
Lorenz Mielke <info@lommix.de>
2024-03-04 12:56:58 +0100

    wip

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,17 +1,34 @@
 return {
 	{
 		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+		},
 		config = function()
-			local dap = require("dap")
+			require("dapui").setup()
+			local dap, dapui = require("dap"), require("dapui")
 
 			vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", numhl = "" })
 			vim.fn.sign_define("DapStopped", { text = "⭐️", texthl = "", linehl = "", numhl = "" })
+
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+
+			dap.listeners.after.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+
+			dap.listeners.after.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
 
 			dap.adapters.godot = {
 				type = "server",
 				host = "127.0.0.1",
 				port = 6006,
 			}
+
 			dap.configurations.gdscript = {
 				{
 					type = "godot",
@@ -21,45 +38,59 @@ return {
 					launch_scene = true,
 				},
 			}
-			dap.adapters.lldb = {
-				type = "executable",
-				command = "lldb-vscode-14",
+
+
+			dap.adapters.codelldb = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = require("mason-registry").get_package("codelldb"):get_install_path() .. "/extension/adapter/codelldb",
+					args = {"--port", "${port}"},
+				},
 				name = "lldb",
 			}
-			dap.configurations.cpp = {
+
+			dap.adapters.gdb = {
+				type = "executable",
+				command = "/usr/bin/gdb",
+				name = "gdb",
+			}
+
+			dap.configurations.rust = {
 				{
 					name = "Launch",
-					type = "lldb",
+					type = "codelldb",
 					request = "launch",
 					program = function()
 						local dir = vim.fn.getcwd()
-						local base_name = vim.fn.fnamemodify(dir, ':t')
+						local base_name = string.lower(vim.fn.fnamemodify(dir, ":t"))
 						local path = dir .. "/target/debug/" .. base_name
+						print(path)
 						return path
 					end,
 					cwd = "${workspaceFolder}",
 					stopOnEntry = false,
 					args = {},
-					runInTerminal = false,
 				},
 			}
-			dap.configurations.c = dap.configurations.cpp
-			dap.configurations.rust = dap.configurations.cpp
-			dap.adapters.coreclr = {
-				type = "executable",
-				command = "netcoredbg",
-				args = {},
-			}
-			dap.configurations.cs = {
-				{
-					type = "coreclr",
-					name = "launch - netcoredbg",
-					request = "launch",
-					program = function()
-						return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/.godot/mono/temp/bin/Debug/", "file")
-					end,
-				},
-			}
+			-- dap.configurations.cpp = {
+			-- 	{
+			-- 		name = "Launch",
+			-- 		type = "lldb",
+			-- 		request = "launch",
+			-- 		program = function()
+			-- 			local dir = vim.fn.getcwd()
+			-- 			local base_name = string.lower(vim.fn.fnamemodify(dir, ':t'))
+			-- 			local path = dir .. "/target/debug/" .. base_name
+			-- 			print(path)
+			-- 			return path
+			-- 		end,
+			-- 		cwd = "${workspaceFolder}",
+			-- 		stopOnEntry = false,
+			-- 		args = {},
+			-- 		runInTerminal = true,
+			-- 	},
+			-- }
 		end,
 		keys = {
 			{ "<F4>", ":DapToggleBreakpoint<CR>" },
@@ -68,7 +99,8 @@ return {
 			{ "<F7>", ":DapStepInto<CR>" },
 			{ "<F8>", ":DapStepOut<CR>" },
 			{ "<F9>", ":DapTerminate<CR>" },
-		}
+			-- { "<F3>", ":lua require('dapui').toggle()<CR>" },
+		},
 	},
 	{
 		"leoluz/nvim-dap-go",
@@ -77,18 +109,9 @@ return {
 		end,
 	},
 	{
-		"rcarriga/nvim-dap-ui",
-		config = function()
-			require("dapui").setup()
-		end,
-		keys = {
-			{ "<F3>", ":lua require('dapui').toggle()<CR>" },
-		}
-	},
-	{
 		"theHamsta/nvim-dap-virtual-text",
 		config = function()
 			require("nvim-dap-virtual-text").setup()
-		end
-	}
+		end,
+	},
 }
