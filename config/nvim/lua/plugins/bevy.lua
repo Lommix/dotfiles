8a09bd05da698e0903e9ae080f2c958a3a132518
Lorenz Mielke <info@lommix.de>
2025-05-10 19:32:22 +0200

    wip

--- config/nvim/lua/plugins/bevy.lua
+++ config/nvim/lua/plugins/bevy.lua
@@ -1,6 +1,6 @@
 return {
 	"lommix/bevy_inspector.nvim",
-	dir = "~/Projects/nvim_plugins/bevy_inspector.nvim",
+	-- dir = "~/Projects/nvim_plugins/bevy_inspector.nvim",
 	dependencies = {
 		"nvim-telescope/telescope.nvim",
 	},
