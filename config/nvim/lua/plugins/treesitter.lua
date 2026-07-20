d9f57f534659837f591cc06a6b74999d2b53a9b7
Lorenz Mielke <info@lommix.de>
2026-07-20 14:09:00 +0200

    wip

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,4 +2,11 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	build = ":TSUpdate",
+	config = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
+		})
+	end,
 }
